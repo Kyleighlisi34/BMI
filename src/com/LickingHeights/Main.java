326cd57a09996863d9c0fbbc691931650dc97a86
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-09-19 10:20:14 -0400

    Made ToDo List
    Organized beginning to do each thing individually

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,35 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //TODO DECLARE VARIABLES
+        double feet, inches, meters, kilograms, bmi;
+        double IN_TO_METERS = 0.0254;
+        Scanner keyboard;
+    //TODO INITIALIZE THE VARIABLES WITH USER INPUT
+
+    //TODO CONVERT VARIABLES TO DO THE EQUATION
+        meters = inches * IN_TO_METERS;
+    //TODO PERFORM BMI CALCULATION
+
+    //TODO PRINT RESULTS
+
+
+    
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
